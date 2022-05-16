9844d840c57ebc3e820f7658e7d1b8907526c8fa
#Jiruu_246 <81064575+Jiruu246@users.noreply.github.com>
2022-05-17 02:05:06 +1000

    mmm

--- edit-orders.php
+++ edit-orders.php
@@ -27,8 +27,7 @@
 			
 			$info= '';
 			$time = date("Y-m-d H:i:s");
-            echo $_GET['EditOrderID'];
-            $sql = "SELECT OrderStatus FROM Orders WHERE OrderID = " . $GET["EditOrderID"];
+            $sql = "SELECT MemberID, OrderStatus, Info FROM Orders WHERE OrderID = " . $_GET['EditOrderID'];
 			$order_result = $conn->query($sql);
 
             $row = $order_result->fetch_assoc();
@@ -114,7 +113,7 @@
 
   <div class="container" style="padding-top:5%;">
   
-    <form action = "update_orders.php" method="post">
+    <form action = "insert_orders.php" method="post">
       <div class="form-row py-3">
         <div class="col-md-6" style="padding: 0% 3%;">
           <h3 style="padding-bottom: 5%;">Billing details</h3>
@@ -131,9 +130,9 @@
 				<lable for="memberid">User ID</label>
 
 					<select class="custom-select overflow-auto" name="memberid" id="memberid" style="margin: 2% 0 2% 0;" required>
-					  <option value="" selected>Open this select menu</option>
 						
 					   <?php
+                        echo '<option selected>' . $row["MemberID"]. '</option>';
 						  while($row = $member_result->fetch_assoc())
 						  {
 							echo "<option value=\"" .$row['MemberID']. "\">" .$row['MemberID']. ' ' .$row['FirstName'].' '.$row['LastName']. "</option>";
@@ -148,7 +147,7 @@
           </div>
           <div class="col" style="padding: 0%;">
             <label for="info" style="padding: 2% 0%;">Additional Information</label>
-            <input class="form-control" type="text" name="info" id="info" placeholder="Note" style="height:30%" required>
+            <?php echo '<input type="text" name="info" class="form-control" id="info" value='.$row["Info"].'>'; ?>
           </div>
         </div>
         <div class="col-md-6" style="padding: 0% 3%;">
